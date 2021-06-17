b1c6db267ec8d6176b4fc63482cf20693fb5e946
alina-delossantos <alina.delossantos@gmail.com>
2021-06-17 09:36:03 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Main: entry point
+ *
+ * Description: print phrase with printf
+ *
+ * Return: 0
+ *
+**/
+
+int main (void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}

27e6030dc8c89c8bfbc4275d8e95f71067383478
alina-delossantos <alina.delossantos@gmail.com>
2021-06-17 09:31:19 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print phrase followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

9180f76259cd749d36aaecfcd383ad8b435fc8e4
alina-delossantos <alina.delossantos@gmail.com>
2021-06-18 08:23:09 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int n = 48;
+while ((n > 47) && (n < 58))
+{
+putchar (n);
+putchar (44);
+n++;
+}
+putchar (10);
+return (0);
+}

a4895794e5393cee14db73bfcf79ac442f305265
alina-delossantos <alina.delossantos@gmail.com>
2021-06-18 07:11:32 -0700

    files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:  prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  int n = 97;
+  while ((n > 96) && (n < 123))
+    {
+      putchar (n);
+	n++;
+    }
+	putchar (10);
+	 return (0);
+}

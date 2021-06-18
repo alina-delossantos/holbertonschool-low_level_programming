2ecfaaa46e9a66999ac4d44b85a1ce758195415e
alina-delossantos <alina.delossantos@gmail.com>
2021-06-18 12:17:09 -0700

    betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,24 +9,23 @@
 
 int main(void)
 {
-    int first = 48, second = 48;
-    for (first = 48; first < 57; first++)
-    {
-        for (second = first + 1; second < 58; second++)
-        {
-            if (second != first)
-            {
-                putchar (first);
-                putchar (second);
-
-                if ((first < 56) || (second < 57))
-                {
-                    putchar (44);
-                    putchar (32);
-                }
-            }
-        }
-    }
-     putchar (10);
-    return (0);
+int first = 48, second = 48;
+for (first = 48; first < 57; first++)
+{
+for (second = first + 1; second < 58; second++)
+{
+if (second != first)
+{
+putchar (first);
+putchar (second);
+if ((first < 56) || (second < 57))
+{
+putchar (44);
+putchar (32);
+}
+}
+}
+}
+putchar (10);
+return (0);
 }

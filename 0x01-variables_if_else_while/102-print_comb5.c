931dcb7454011b9f49f2e6159e2e2754e542d659
alina-delossantos <alina.delossantos@gmail.com>
2021-06-19 15:43:44 -0700

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:  prints all possible different combinations of three digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int first, second, third, fourth;
+for (first = 48; first < 58; first++)
+    {
+for (second = 48; second < 57; second++)
+        {
+for (third = second + 1; third < 58; third++)
+{
+for (third = 48; third < 58; third++)
+{
+for (fourth = second + 1; third < 58; third++)
+{
+    putchar(first);
+    putchar (second);
+    putchar (32);
+    putchar (third);
+    putchar(fourth);
+    if (first < 57 || second < 56 || third < 57 || fourth < 57)
+    {
+putchar (44);
+putchar(32);
+    }
+}
+}
+}
+}
+}
+putchar (10);
+return (0);
+}

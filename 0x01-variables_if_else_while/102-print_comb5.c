398796a84f5a71c21afa30d29325c17b5e2a236b
alina-delossantos <alina.delossantos@gmail.com>
2021-06-19 16:17:10 -0700

    betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,24 @@ int main(void)
 {
 int first, second, third, fourth;
 for (first = 48; first < 58; first++)
-    {
+{
 for (second = 48; second < 58; second++)
-        {
+{
 for (third = 48; third < 58; third++)
 {
 for (fourth = second + 1; fourth < 58; fourth++)
 {
-    putchar(first);
-    putchar (second);
-    putchar (32);
-    putchar (third);
-    putchar(fourth);
-    if (first < 57 || second < 56 || third < 57 || fourth < 57)
-    {
+putchar(first);
+putchar (second);
+putchar (32);
+putchar (third);
+putchar(fourth);
+if (first < 57 || second < 56 || third < 57 || fourth < 57)
+{
 putchar (44);
 putchar(32);
-    }
-    }
+}
+}
 }
 }
 }

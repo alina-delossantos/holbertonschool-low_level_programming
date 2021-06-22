5194f79d8caa26a1141b630d699925b3b477ce5b
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 09:59:37 -0700

    betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
 
 /**
- * _islower - checks for lowercase
+ * _abs -  computes the absolute value of an integer
  *
- * @c: int
+ * @n: int
  *
  * Return: void
  */
 
 int _abs(int n)
 {
-
-  if (n > 0)
-    return (n);
-  else if (n < 0)
-    return (-n);
-  else
-    return (0);
+if (n > 0)
+return (n);
+else if (n < 0)
+return (-n);
+else
+return (0);
 }

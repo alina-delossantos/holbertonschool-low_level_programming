04a7a747b16fbc09b72fa292cab16f57cdc30990
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 08:59:33 -0700

    changed if

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-  int i, letter;
-  for (i = 0; i < 11; i++)
-    {
-      for (letter = 97; letter < 123; letter++)
-	{
-	_putchar(letter);
-	}
-      _putchar(10);
-    }
+int i, letter;
+for (i = 0; i < 10; i++)
+{
+for (letter = 97; letter < 123; letter++)
+{
+_putchar(letter);
+}
+_putchar(10);
+}
 }

e1c36b132f21ba6ce9248fb137106655573e4d3b
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 09:35:11 -0700

    betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,16 +10,16 @@
 
 int _isalpha(int c)
 {
-  if ((c > 64) && (c < 91))
-    {
-      return (1);
-    }
-  if ((c > 96) && (c < 123))
-    {
-    return (1);
-    }
-  else
-    {
-    return (0);
-    }
+if ((c > 64) && (c < 91))
+{
+return (1);
+}
+if ((c > 96) && (c < 123))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }

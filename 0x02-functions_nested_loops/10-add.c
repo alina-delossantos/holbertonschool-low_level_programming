e179fc1fb0149a7bc0e2f0c75e1fdc57eec7e4c2
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 13:32:13 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * add - adds two int
+ *
+ * @n1: int
+ * @n2: int
+ *
+ * Return: void
+ */
+
+int add(int n1, int n2)
+{
+  int sum;
+
+  sum = n1 + n2;
+
+  return (sum);
+}

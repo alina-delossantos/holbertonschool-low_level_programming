b458aa84fb880d3c9a5d8bf4bc71a42dbf0a3baf
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 13:16:43 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,36 +9,36 @@
 
 void times_table(void)
 {
-  int row, column, product, one, ten; 
+int row, column, product, one, ten; 
 
-  for (row = 0; row < 10; row++)
-    {
-    for (column = 0; column < 10; column++)
-      {
+for (row = 0; row < 10; row++)
+{
+for (column = 0; column < 10; column++)
+{
 
-	product = column * row;
+product = column * row;
 	
-	if (column == 0)
-	  {
-	    _putchar(48);
-	  }
-	if (product < 10 && column > 0)
-	  {
-	    _putchar(44);
-	    _putchar(32);
-	    _putchar(32);
-	    _putchar(product + 48);
-	  }
-	if (product >= 10 && column > 0)
-	  {
-	    ten = ((product / 10) + 48);
-	    one = ((product % 10) + 48);
-	    _putchar(44);
-	    _putchar(32);
-	    _putchar(ten);
-	    _putchar(one);
-	  }
-      }
-    _putchar(10);
-      }
+if (column == 0)
+{
+_putchar(48);
+}
+if (product < 10 && column > 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+_putchar(product + 48);
+}
+if (product >= 10 && column > 0)
+{
+ten = ((product / 10) + 48);
+one = ((product % 10) + 48);
+_putchar(44);
+_putchar(32);
+_putchar(ten);
+_putchar(one);
+}
+}
+_putchar(10);
+}
 }

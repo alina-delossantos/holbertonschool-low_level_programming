1298b9eb3fe53ef2e2c4b148200d7c679462a818
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 09:33:57 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for lowercase
+ *
+ * @c: int
+ *
+ * Return: void
+ */
+
+int _isalpha(int c)
+{
+  if ((c > 64) && (c < 91))
+    {
+      return (1);
+    }
+  if ((c > 96) && (c < 123))
+    {
+    return (1);
+    }
+  else
+    {
+    return (0);
+    }
+}

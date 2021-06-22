381969e9cac34767f889e7ad2bb420385972ffb5
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 10:29:36 -0700

    betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,18 +10,18 @@
 
 void jack_bauer(void)
 {
-  int hours, minutes;
+int hours, minutes;
 
-  for (hours = 0; hours < 24; hours++)
-    {
-      for (minutes = 0; minutes < 60; minutes++)
-	{
-	    _putchar((hours / 10) + 48);
-	  _putchar ((hours % 10) + 48);
-	  _putchar(58);
-	  _putchar((minutes / 10) + 48);
-	  _putchar ((minutes % 10) + 48);
-	  _putchar(10);
-	}
-    }
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar((hours / 10) + 48);
+_putchar ((hours % 10) + 48);
+_putchar(58);
+_putchar((minutes / 10) + 48);
+_putchar ((minutes % 10) + 48);
+_putchar(10);
+}
+}
 }

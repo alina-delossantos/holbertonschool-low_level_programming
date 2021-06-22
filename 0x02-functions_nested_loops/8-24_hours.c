d7b1093a5e31eba516ea1b4fbbc1e0d4492d0feb
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 10:27:52 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ *
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+  int hours, minutes;
+
+  for (hours = 0; hours < 24; hours++)
+    {
+      for (minutes = 0; minutes < 60; minutes++)
+	{
+	    _putchar((hours / 10) + 48);
+	  _putchar ((hours % 10) + 48);
+	  _putchar(58);
+	  _putchar((minutes / 10) + 48);
+	  _putchar ((minutes % 10) + 48);
+	  _putchar(10);
+	}
+    }
+}

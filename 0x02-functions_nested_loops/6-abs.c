fc3bb33e0e86ae371ac44f9b73095b5603146ec6
alina-delossantos <alina.delossantos@gmail.com>
2021-06-22 09:57:41 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * @c: int
+ *
+ * Return: void
+ */
+
+int _abs(int n)
+{
+
+  if (n > 0)
+    return (n);
+  else if (n < 0)
+    return (-n);
+  else
+    return (0);
+}

e733d91dcd9ef081a0bfff8ccfe9911b4b9c3c86
alina-delossantos <alina.delossantos@gmail.com>
2021-06-24 15:39:43 -0700

    fhf

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,18 @@
 void print_square(int size)
 {
 	int i, j;
-
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
-			if (size >= 0)
 			{
 				_putchar('#');
 			}
+		_putchar(10);
+	}
+}
+	else {
 		_putchar(10);
 	}
 }

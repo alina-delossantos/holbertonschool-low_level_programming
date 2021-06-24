0a11f7191e71ed579df5175c7d4cc89221110e40
alina-delossantos <alina.delossantos@gmail.com>
2021-06-24 15:07:59 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,24 +12,25 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
-		_putchar(10);
-
 	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
 		{
 		for (j = 0; j < n; j++)
 		{
-			if (j == i)
-			{
-				_putchar(92);
-			}
-			else
-				_putchar(32);
+		if (j < i)
+		{
+			_putchar(32);
 		}
-		_putchar(10);
+		else if (j == i)
+		{
+			_putchar(92);
+			_putchar(10);
 		}
-
-}
+		}
+		}
+		else
+			_putchar(10);
+	}
+	_putchar(10);
 }

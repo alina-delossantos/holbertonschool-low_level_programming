fdae1a2c8797fb6f412d1d81fa8f5eeb52cf1cdd
alina-delossantos <alina.delossantos@gmail.com>
2021-06-24 14:39:40 -0700

    cond

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar(10);
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
 		{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j < n; j++)
 		{
 			if (j == i)
 			{

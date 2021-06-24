2698ce8d46b70ab85ea50e33e8ca60b4ebe49b32
alina-delossantos <alina.delossantos@gmail.com>
2021-06-24 10:06:58 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}

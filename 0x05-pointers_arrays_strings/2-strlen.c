a5853ff35dfaa6d5abc21de422ebd4e8cdfc6d72
alina-delossantos <alina.delossantos@gmail.com>
2021-06-28 17:42:53 -0700

    betty

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: pointer
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}

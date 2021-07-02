f0aaae6001738dbcb18b21c428026ad5b8cc9ce0
alina-delossantos <alina.delossantos@gmail.com>
2021-07-01 19:01:02 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @str: string
+ *
+ * Return: char
+ */
+
+char *rot13(char *str)
+{
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+	}
+	}
+	return (str);
+}

067a26cb9cb0efa6eb2d85592da9a57044622889
alina-delossantos <alina.delossantos@gmail.com>
2021-07-01 18:34:48 -0700

    header and t7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - entry point
+ *
+ * @str: string
+ *
+ * Return: char
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char s2[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return(str);
+}

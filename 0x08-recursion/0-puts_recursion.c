7bb5cfec71017e2452ecdd0b58a294a41c985312
alina-delossantos <alina.delossantos@gmail.com>
2021-07-08 14:15:08 -0700

    fhfhf

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * Return: pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar(10);
+		return;
+	}
+}

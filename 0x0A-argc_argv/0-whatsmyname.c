4f689becd98fe2ac4d6664ca42bd404212633181
alina-delossantos <alina.delossantos@gmail.com>
2021-07-09 10:09:51 -0700

    zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

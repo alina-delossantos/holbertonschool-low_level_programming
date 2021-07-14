5338a95d38bf5ef84802433afe95001480df7ebc
alina-delossantos <alina.delossantos@gmail.com>
2021-07-13 17:22:43 -0700

    ffhhf

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,26 +10,29 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, **grid;
+	int **grid, i;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **) calloc(height, sizeof(int *));
+
+	grid =  (int **) malloc(height * sizeof(int *));
+
 	if (grid == NULL)
-		return(NULL);
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = calloc(width, sizeof(int));
+		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			while (i >= 0)
 			{
 				free(grid[i]);
-			i--;
+				i--;
 			}
+			free(grid);
+			return (NULL);
 		}
-		free(grid);
-		return(NULL);
 	}
-	return(grid);
+	return (grid);
 }

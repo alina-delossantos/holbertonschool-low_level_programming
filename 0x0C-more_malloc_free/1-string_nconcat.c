850711c7c8f46478e752bf725a0eab7670a1cb09
alina-delossantos <alina.delossantos@gmail.com>
2021-07-16 17:19:52 -0700

    betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -50,7 +50,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		size2 = n;
-	sizeconc = size1+ n + 1;
+	sizeconc = size1 + n + 1;
 	}
 	ptr = malloc(sizeconc);
 	if (ptr == NULL)

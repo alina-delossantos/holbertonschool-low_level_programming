2c271405bf1c154622c02e0dc9845c1802a61b79
alina-delossantos <alina.delossantos@gmail.com>
2021-07-22 10:02:31 -0700

    fhfhfh

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: struct
+ * @name: char
+ * @age: float
+ * @owner: char pointer
+ * Return: void
+ */

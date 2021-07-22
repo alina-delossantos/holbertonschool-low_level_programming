ef75d17d7779664d33fddbb406da5617463f140a
alina-delossantos <alina.delossantos@gmail.com>
2021-07-22 10:26:27 -0700

    fhhf

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,28 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: struct
- * @name: char
- * @age: float
- * @owner: char pointer
- * Return: void
+ * sum_them_all - sum of all its parameters
+ * @n: const unsigned int
+ * Return: int
  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arguments;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(arguments, n);
+
+	for (sum = 0, i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(arguments, int);
+	}
+	va_end(arguments);
+
+	return (sum);
+}

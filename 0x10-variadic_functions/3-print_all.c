b859423fcdede02ff45f4fb11e5d0bc836ffb500
alina-delossantos <alina.delossantos@gmail.com>
2021-07-22 18:04:29 -0700

    ghg

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: dsjd
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}

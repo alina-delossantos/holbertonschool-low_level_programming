7d0e42f5e2123355188c9d2a10657fcbac4058e6
alina-delossantos <alina.delossantos@gmail.com>
2021-08-03 14:49:21 -0700

    elcommit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elemnts within list
+ * @h: head
+ * Return: nbr of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t size;
+
+	for(size = 0; h != NULL; size++)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+	}
+	return(size);
+}

5912e4fab8e2400c7f1dfa3ce828953c7c11d65a
alina-delossantos <alina.delossantos@gmail.com>
2021-08-03 14:51:12 -0700

    betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t size;
 
-	for(size = 0; h != NULL; size++)
+	for (size = 0; h != NULL; size++)
 	{
 		printf("%d\n", (*h).n);
 		h = (*h).next;
 	}
-	return(size);
+	return (size);
 }

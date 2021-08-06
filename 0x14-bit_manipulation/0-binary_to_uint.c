fb7367df09ab09deccad1af1225e5b524cf7a2fa
alina-delossantos <alina.delossantos@gmail.com>
2021-08-05 17:35:53 -0700

    fjkjfjf

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,61 +1,30 @@
 #include "holberton.h"
 
 /**
- * _strlen - string size
- * @s: strin
- * Return: size
- */
-
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
- * power - xp
- * @base: base
- * @exp: xp
- * Return: int
- */
-
-int power(int base, int exp)
-{
-	int i, num;
-
-	num = 1;
-	for (i = 0; i < exp; ++i)
-		num *= base;
-
-	return (num);
-}
-
-/**
  * binary_to_uint - bin to int
- * @b: bin
- * Return: int
+ * @b: strin
+ * Return: decimal
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	int length, i;
+	unsigned int dec = 0;
+	int i, bit = 1;
 
-	sum = 0;
-	if (b == NULL)
-		return (sum);
-	length = _strlen(b);
-	for (i = length - 1; i >= 0; i--)
+	if (b == 0)
+		return (0);
+	for (i = 0; b[i]; i++)
+		;
+	for (i--; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		sum += (b[i] - '0') * power(2, length - i - 1);
+		else if (b[i] == '1')
+		{
+			dec += bit;
+			bit = bit * 2;
+		}
+		else
+			bit = bit * 2;
 	}
-	return (sum);
+return (dec);
 }

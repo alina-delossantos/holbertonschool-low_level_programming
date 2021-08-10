bf7ad4e6522891abe0eb23665313550617b9e808
alina-delossantos <alina.delossantos@gmail.com>
2021-08-10 11:14:56 -0700

    betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,26 +12,26 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file_desc;
 	unsigned int size;
 	ssize_t writtenbytes;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 		return (-1);
-	
+
 	file_desc = open(filename, O_WRONLY | O_APPEND);
-	
+
 	if (file_desc == -1)
 		return (-1);
-	
+
 	size = strlen(text_content);
-	
+
 	writtenbytes = write(file_desc, text_content, size);
-	
+
 	if (writtenbytes == -1)
 		return (-1);
-	
+
 	close(file_desc);
-	
+
 	return (1);
 }

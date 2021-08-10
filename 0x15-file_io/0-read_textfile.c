ecb9d6eae1e24cf77a468aa664f20ae814664c2b
alina-delossantos <alina.delossantos@gmail.com>
2021-08-10 06:47:18 -0700

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,23 +12,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file_desc;
 	char *buf;
 	ssize_t readbytes, writtenbytes;
-	
+
 	if (filename == 0)
-		return(0);
+		return (0);
 	file_desc = open(filename, O_RDONLY);
-	
+
 	if (file_desc == -1)
-		return(0);
+		return (0);
 
 	buf = malloc(sizeof(char) * letters + 1);
 
 	if (buf == NULL)
-		return(0);
+		return (0);
 
 	readbytes = read(file_desc, buf, letters);
 
 	if (readbytes == -1)
-		return(0);
+		return (0);
 
 	buf[letters + 1] = '\0';
 
@@ -37,9 +37,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	writtenbytes = write(STDOUT_FILENO, buf, readbytes);
 
 	if (writtenbytes == -1)
-		return(0);
+		return (0);
 
 	free(buf);
 
-	return(readbytes);
+	return (readbytes);
 }

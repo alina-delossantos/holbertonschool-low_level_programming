1fc77eaf982d13ed4acb850f90f7d30e0d85de06
alina-delossantos <alina.delossantos@gmail.com>
2021-08-10 10:06:47 -0700

    fhfhhf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
  * create_file - creates a file
  * @filename: file name
  * @text_content: string
@@ -28,14 +10,14 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int file_desc, size, writtenbytes;
-	
+
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (file_desc == -1)
-		return(-1);
+		return (-1);
 
 	if (text_content == NULL)
 	{
@@ -44,12 +26,12 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		size = _strlen(text_content);
+		size = strlen(text_content);
 	}
 	writtenbytes = write(file_desc, text_content, size);
 
-	if(writtenbytes == -1)
-		return(-1);
-	close (file_desc);
-	return(-1);
+	if (writtenbytes == -1)
+		return (-1);
+	close(file_desc);
+	return (-1);
 }

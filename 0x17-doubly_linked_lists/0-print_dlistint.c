4563f6df33e554535f02b35f3c7652f0d0aec115
root <alina.delossantos@gmail.com>
2022-01-09 21:48:33 +0000

    fhfhf

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 
-  int counter;
+	int counter;
 
-  for (counter = 0; h; counter++)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
-  return (counter);
+	for (counter = 0; h; counter++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (counter);
 }

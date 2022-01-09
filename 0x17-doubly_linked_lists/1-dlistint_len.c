c71e98ed94ff51f948dc89954c39f3661c876adf
root <alina.delossantos@gmail.com>
2022-01-09 21:51:22 +0000

    fjfjf

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: head
+ *
+ * Return: node nbr
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter;
+
+	for (counter = o; h; counter++)
+	{
+		h = h->next;
+	}
+	return (counter);
+}

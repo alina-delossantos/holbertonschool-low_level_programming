47fb9f7dcfddf947753bea0e23473c97ed181737
root <alina.delossantos@gmail.com>
2022-01-15 18:07:35 +0000

    cfkck

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,48 +10,44 @@
 
 void print_times_table(int n)
 {
-	int row, column, product, one, ten, hundred;
+	int row, column, product;
 
 	if (n >= 0 && n < 15)
 	{
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
-
-			product = column * row;
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
 
 				if (column == 0)
-				{
-					_putchar(48);
-				}
+					_putchar('0');
 				else if (product < 10)
-		{
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			_putchar(32);
-			_putchar(product + 48);
-		}
-				else if (product >= 10 && product <100)
-		{
-			ten = ((product / 10) + 48);
-		one = ((product % 10) + 48);
-		_putchar(44);
-		_putchar(32);
-			_putchar(32);
-		_putchar(ten);
-		_putchar(one);
-		}
-				else if (product >= 100)
-		{
-			hundred = ((product / 100) + 48);
-			_putchar(44);
-			_putchar(32);
-			_putchar(hundred);
-			_putchar(ten);
-			_putchar(one);
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-		_putchar(10);
-}
 }

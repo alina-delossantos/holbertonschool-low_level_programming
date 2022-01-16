8a662b5e6c23d60201b1435e6193cfb5ad89db15
root <alina.delossantos@gmail.com>
2022-01-16 02:02:44 +0000

    djdjd

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,15 +13,15 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
+		_putchar(10);
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
-			_putchar(' ');
+			_putchar(32);
 		}
-		_putchar('\\');
-		_putchar('\n');
+		_putchar(92);
+		_putchar(10);
 	}
 
 }

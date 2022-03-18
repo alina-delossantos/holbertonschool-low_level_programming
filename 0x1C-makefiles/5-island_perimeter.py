0976399eaed23d717179d96d187db8e879a6f00a
root <alina.delossantos@gmail.com>
2022-03-17 18:45:07 -0700

    5testnew

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+fx  that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+
+    size = 0
+    if grid is None:
+        return size
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    size += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    size += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    size += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    size += 1
+    return(size)
